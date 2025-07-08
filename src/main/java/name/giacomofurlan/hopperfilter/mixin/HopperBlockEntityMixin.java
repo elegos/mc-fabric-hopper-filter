6f117f60cfc4ccd28cc919e10e57977d18d0fc35
Giacomo 'Mr. Wolf' Furlan <git@giacomofurlan.name>
2025-07-09 00:19:01 +0200

    Prevent hopper item insertion if below block is a filtered hopper with the item set to pass

--- src/main/java/name/giacomofurlan/hopperfilter/mixin/HopperBlockEntityMixin.java
+++ src/main/java/name/giacomofurlan/hopperfilter/mixin/HopperBlockEntityMixin.java
@@ -1,6 +1,8 @@
 package name.giacomofurlan.hopperfilter.mixin;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,11 +10,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import name.giacomofurlan.hopperfilter.HopperFilterStorage;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.Hopper;
 import net.minecraft.block.entity.HopperBlockEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 
 @Mixin(HopperBlockEntity.class)
 public class HopperBlockEntityMixin {
@@ -41,6 +46,36 @@ public class HopperBlockEntityMixin {
         boolean allowed = filter.stream().anyMatch(f -> ItemStack.areItemsEqual(f, stack));
         if (!allowed) {
             cir.setReturnValue(false);
+        }
+    }
+
+    @Inject(method = "insert", at = @At("HEAD"), cancellable = true)
+    private static void onInsert(World world, BlockPos pos, HopperBlockEntity blockEntity, CallbackInfoReturnable<Boolean> cir) {
+        // If the block below is a filtered hopper and the first non-empty stack of the inventory is in the filter list,
+        // stop the insertion
+        BlockEntity belowBlock = world.getBlockEntity(pos.down());
+        if (belowBlock instanceof HopperBlockEntity belowHopper) {
+            List<ItemStack> filter = HopperFilterStorage.getFilter(belowHopper);
+            if (filter == null || filter.isEmpty()) {
+                return;
+            }
+
+            Optional<ItemStack> nextStackToInsertOpt = IntStream.range(0, blockEntity.size())
+                .mapToObj(blockEntity::getStack)
+                .filter(stack -> !stack.isEmpty())
+                .findFirst();
+            
+            if (!nextStackToInsertOpt.isPresent()) {
+                return;
+            }
+
+            ItemStack nextStackToInsert = nextStackToInsertOpt.get();
+
+            boolean filterMatch = filter.stream().anyMatch(stack -> ItemStack.areItemsEqual(stack, nextStackToInsert));
+            if (filterMatch) {
+                cir.setReturnValue(false);
+                cir.cancel();
+            }
         }
     }
 }
